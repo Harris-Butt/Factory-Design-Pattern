2df1f322a7ffd1b6441dd9f1bafbc4e1f39f1bab
harris <harrisneo@gmail.com>
2020-02-16 19:47:29 +0500

    Add project files.

---
+++ FactoryDesginPattern/CreditCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryDesginPattern
+{
+    public interface CreditCard
+    {
+        string GetCardType();
+        int GetCardLimit();
+        int GetAnnualCharge();
+    }
+}
---
+++ FactoryDesginPattern/MoneyBack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryDesginPattern
+{
+    class MoneyBack : CreditCard
+    {
+        public int GetAnnualCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCardLimit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCardType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
